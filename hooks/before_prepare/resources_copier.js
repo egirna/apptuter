7702fd50f3a7dba00f95d3bb5048e57f6ca76761
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-05-27 04:33:17 +0200

    Updating before_prepare

--- hooks/before_prepare/resources_copier.js
+++ hooks/before_prepare/resources_copier.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').execSync,
-    child;
+var fs = require('fs-extra');
+var del = require('del');
+path = require('path');
 
-child = exec('npm install fs-extra',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+var rootDir = process.cwd();
+del.sync(path.join(rootDir, "platforms/android/res", "/drawable*/"));
+try {
+    fs.copySync(rootDir + '/res/android', rootDir + '/platforms/android/res')
+    console.log("success copy android res!")
+} catch (err) {
+    console.error(err)
+}
 
-        var fs = require('fs-extra');
 
-        var rootDir = process.cwd();
-        console.log(fs.toString())
-        try {
-            fs.copySync(rootDir + '/res/android', rootDir + '/platforms/android/res')
-            console.log("success copy android res!")
-        } catch (err) {
-            console.error(err)
-        }
-    });
